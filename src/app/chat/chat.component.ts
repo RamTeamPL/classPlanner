da2ddf1a6764fa9f286a546e970836d2d923ca96
Qrchy <130994567+Qrchy@users.noreply.github.com>
2023-10-29 17:22:33 +0100

    chat header

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-
+  
 }
