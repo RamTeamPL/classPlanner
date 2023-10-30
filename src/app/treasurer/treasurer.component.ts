610add40d9b9f1c1ace8d72a8dd7a51fb6bee678
Krejzonek <125279077+Krejzonek@users.noreply.github.com>
2023-10-30 20:35:01 +0100

    cos

--- src/app/treasurer/treasurer.component.ts
+++ src/app/treasurer/treasurer.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-treasurer',
   templateUrl: './treasurer.component.html',
-  styleUrls: ['./treasurer.component.css']
+  styleUrls: ['./treasurer.component.css'],
+  
+  
 })
 export class TreasurerComponent {
-
+  centered = false;
+  disabled = false;
+  unbounded = false;
+  color = "rgba(114, 114, 114, 0.495)";
+  panelOpenState = false;
 }
+
