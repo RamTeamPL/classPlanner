3c505726298b737c6196a17456ef8af268f58b41
thebigr0l <karololowiecki@gmail.com>
2023-11-03 17:33:55 +0100

    more in navbar

--- src/app/more/more.component.ts
+++ src/app/more/more.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./more.component.css']
 })
 export class MoreComponent {
-
+  centered = false;
+  disabled = false;
+  unbounded = false;
+  color = "rgba(114, 114, 114, 0.495)";
+  users = [
+    {id: 0, name: 'Bartek', lastname: 'Olszyński', avatar:'../assets/img/icons8-male-user-96.png'},
+    {id: 1, name: 'Miłosz', lastname: 'Kwiat', avatar:'../assets/img/icons8-male-user-96.png'}
+  ]
 }
