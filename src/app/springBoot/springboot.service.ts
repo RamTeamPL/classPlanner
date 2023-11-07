324b4c4308bbccf410e00c252118e09f1fd26301
Mineomi <mineomi1234@gmail.com>
2023-11-07 18:14:02 +0100

    zmiany w łączeniu do bazy danych

--- src/app/springBoot/springboot.service.ts
+++ src/app/springBoot/springboot.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class SpringbootService {
   memberId: any = 9;
-  springBootIp: any =  "192.168.1.110";
+  springBootIp: any =  "127.0.0.1";
+  //springBootIp: any =  "192.168.1.110";
 
   getMemberId(){
     return this.memberId;
